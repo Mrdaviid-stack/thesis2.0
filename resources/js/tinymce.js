57648e1cb51c1d30315f8c82c25b4f4ba5b03421
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2024-10-25 14:58:54 +0800

    UPDATE - remove api-key

--- resources/js/tinymce.js
+++ resources/js/tinymce.js
@@ -2,7 +2,6 @@ import axios from "axios"
 
 tinymce.init({
     selector: '.tinymce',
-    license_key: 'awa70o85dsvanz62tuz4f8l7u3xtw3vt8dqtjjr5yjf4rgdu',
     menubar: "file edit view insert format tools table help",
     plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
     toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | bootstrapLayout',
