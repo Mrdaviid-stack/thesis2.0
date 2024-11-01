8bef951bfdc25944230668510c36f7cd6372327f
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2024-11-01 17:33:27 +0800

    UPDATE - remove unused import

--- app/modules/CMS/Admin/controllers/users_controller.ts
+++ app/modules/CMS/Admin/controllers/users_controller.ts
@@ -1,6 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
-import BasesController from "../../../Core/controllers/bases_controller.js"
 import User from "../models/user.js"
 import { UserValidationSchema } from "../validators/user.js"
 import Group from '../models/group.js'
