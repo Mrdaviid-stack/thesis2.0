cc614ddd17741791c4ea76c63c6edecb6aca605b
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2024-12-03 10:53:51 +0800

    Build: purchase history

--- app/modules/CMS/Customers/controllers/histories_controller.ts
+++ app/modules/CMS/Customers/controllers/histories_controller.ts
@@ -1,9 +1,20 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import Transaction from '../../Websites/models/transaction.js'
+import db from '@adonisjs/lucid/services/db'
 
 export default class HistoriesController {
 
-    async index({ view }: HttpContext) {
-        return view.render('pages/cms/customers/history/history_index')
+    async index({ view, request }: HttpContext) {
+        
+        const page = request.input('page', 1)
+
+        const histories = await Transaction.query().select('id','order_id','reference','delivery_status','payment_method')
+            .preload('order', (orderQuery) => orderQuery.select(db.raw('CONCAT(first_name, \' \', last_name) AS fullname'))).paginate(page, 10)
+
+        const baseUrl = request.url().split('?',1)[0]
+        histories?.baseUrl(baseUrl)
+
+        return view.render('pages/cms/customers/history/history_index', { histories })
     }
 
 }
