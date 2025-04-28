912ece22fc0b03a18a518ee705901a1a4df895d4
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-04-28 11:57:56 +0800

    feature: add product name

--- app/modules/CMS/Customers/controllers/histories_controller.ts
+++ app/modules/CMS/Customers/controllers/histories_controller.ts
@@ -9,7 +9,15 @@ export default class HistoriesController {
         const page = request.input('page', 1)
 
         const histories = await Transaction.query().select('id','order_id','reference','delivery_status','payment_method')
-            .preload('order', (orderQuery) => orderQuery.select(db.raw('CONCAT(first_name, \' \', last_name) AS fullname'))).paginate(page, 10)
+            .preload('order', (orderQuery) => {
+                orderQuery
+                    .select(db.raw('CONCAT(first_name, \' \', last_name) AS fullname'))
+                    .preload('orderItems', (orderItemQuery) => 
+                        orderItemQuery.preload('productVariant', (productVariantQuery) => {
+                            productVariantQuery.preload('product', (productQuery) => productQuery.select('*'))
+                        })
+                )
+            }).paginate(page, 10)
 
         const baseUrl = request.url().split('?',1)[0]
         histories?.baseUrl(baseUrl)
