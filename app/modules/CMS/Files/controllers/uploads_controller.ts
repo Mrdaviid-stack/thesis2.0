8a26cff3a75d33a3699a115d75f6000c3eeb7f8e
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-05-17 17:17:08 +0800

    bugfix: code debugging

--- app/modules/CMS/Files/controllers/uploads_controller.ts
+++ app/modules/CMS/Files/controllers/uploads_controller.ts
@@ -3,23 +3,28 @@ import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
 
 export default class UploadsController {
-    async uploads({ request, response }: HttpContext) {
-        const image = request.file('image')
-        if(image) {
-            const date = new Date()
-            await image.move(app.makePath(`public/uploads/${date.getFullYear()}/${date.getMonth()}/${date.getDate()}/`), {
-                name: `${cuid()}.${image.extname}`
-            })
+  async uploads({ request, response }: HttpContext) {
+    const image = request.file('image')
+    if (image) {
+      const date = new Date()
+      await image.move(
+        app.makePath(`public/uploads/${date.getFullYear()}/${date.getMonth()}/${date.getDate()}/`),
+        {
+          name: `${cuid()}.${image.extname}`,
         }
-
-        console.log(image?.filePath);
-        const index = image?.filePath?.indexOf('uploads\\')
-        const extractedPath = image?.filePath?.slice(index)
-        console.log(extractedPath, 'image path')
-        //default -4
-        return response.status(200).json({
-            //location: `\\${extractedPath}`
-            location: `/uploads/${image?.filePath?.split('\\').slice(-4).join('/')}`
-        })
+      )
     }
+
+    console.log(image?.filePath)
+    //const index = image?.filePath?.indexOf('uploads\\')
+    //const extractedPath = image?.filePath?.slice(index)
+    //console.log(/uploads/${image?.filePath?.split('\\').slice(-4).join('/'), 'image path')
+    //default -4
+    const path = `/uploads/${image?.filePath?.split('\\').slice(-4).join('/')}`
+    console.log(path)
+    return response.status(200).json({
+      //location: `\\${extractedPath}`
+      location: `/uploads/${image?.filePath?.split('\\').slice(-4).join('/')}`,
+    })
+  }
 }
