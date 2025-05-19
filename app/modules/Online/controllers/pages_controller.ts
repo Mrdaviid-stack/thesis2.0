4a7f9a4fff2ff9e35d56fc64d2ce26ed7be40c48
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-05-19 22:38:05 +0800

    build: remove unused module

--- app/modules/Online/controllers/pages_controller.ts
+++ app/modules/Online/controllers/pages_controller.ts
@@ -2,7 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Page from '../../CMS/Websites/models/page.js'
 
 export default class PagesController {
-  async index({ view, response }: HttpContext) {
+  async index({ view }: HttpContext) {
     const page = await Page.findByOrFail('slug', 'home')
     const customPages = await Page.query().select('slug')
 
