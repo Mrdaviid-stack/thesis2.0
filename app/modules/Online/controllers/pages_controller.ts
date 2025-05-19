76ad2966049d65cc4180f96d00c4c74dc9bef9be
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-05-19 22:35:47 +0800

    feature: custom page

--- app/modules/Online/controllers/pages_controller.ts
+++ app/modules/Online/controllers/pages_controller.ts
@@ -2,10 +2,18 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Page from '../../CMS/Websites/models/page.js'
 
 export default class PagesController {
+  async index({ view, response }: HttpContext) {
+    const page = await Page.findByOrFail('slug', 'home')
+    const customPages = await Page.query().select('slug')
 
-    async index({ view }: HttpContext) {
-        const page = await Page.findByOrFail('slug', 'home')
-        return view.render('pages/online/landing', { page })
-    }
-
+    return view.render('pages/online/landing', { page, customPages })
+  }
+  async slugPage({ view, params }: HttpContext) {
+    const page = await Page.findByOrFail('slug', params.slug)
+    return view.render('pages/online/slug', { page })
+  }
+  async getCustomPages({ response }: HttpContext) {
+    const customPages = await Page.query().select('slug')
+    return response.status(200).json({ pages: customPages })
+  }
 }
