8a205aaea669e9015e5dfb75a49e4c5b776b0ce7
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-05-20 22:42:09 +0800

    bugfix: adjust stocking beahavior

--- app/modules/CMS/Websites/controllers/restockings_controller.ts
+++ app/modules/CMS/Websites/controllers/restockings_controller.ts
@@ -3,35 +3,39 @@ import ProductVariant from '../models/product_variant.js'
 import historyService from '../../Reports/services/historyServices.js'
 
 export default class RestockingsController {
-    async index({ view, request, auth }: HttpContext) {
-        const page = request.input('page', 1)
-        const records = await ProductVariant.query().preload('product').select('*').paginate(page, 10)
+  async index({ view, request, auth }: HttpContext) {
+    const page = request.input('page', 1)
+    const records = await ProductVariant.query().preload('product').select('*').paginate(page, 10)
 
-        const baseUrl = request.url().split('?',1)[0]
-        records?.baseUrl(baseUrl)
+    const baseUrl = request.url().split('?', 1)[0]
+    records?.baseUrl(baseUrl)
 
-        await historyService(auth.user?.firstname!, 'Visit Restocking Page')
+    await historyService(auth.user?.firstname!, 'Visit Restocking Page')
 
-        return view.render('pages/cms/websites/restocking/restocking_index', { records: records?.serialize(), paginations: records })
-    }
-    async form({ view, params }: HttpContext) {
-        console.log(params)
+    return view.render('pages/cms/websites/restocking/restocking_index', {
+      records: records?.serialize(),
+      paginations: records,
+    })
+  }
+  async form({ view, params }: HttpContext) {
+    console.log(params)
 
-        const query = await ProductVariant.findByOrFail('id', params.id);
+    const query = await ProductVariant.findByOrFail('id', params.id)
 
-        console.log(query)
+    console.log(query)
 
-        return view.render('pages/cms/websites/restocking/restocking_form', {
-            record: {
-                id: query.id,
-                stock: query.stock
-            }
-        })
-    }
-    async store({ request, response }: HttpContext) {
-        let { id, stock } = request.body()
-        const query = await ProductVariant.findByOrFail('id', id);
-        await query.merge({ stock: stock }).save()
-        return response.status(200).json({ message: 'Restock Saved!' })
-    }
+    return view.render('pages/cms/websites/restocking/restocking_form', {
+      record: {
+        id: query.id,
+        stock: query.stock,
+      },
+    })
+  }
+  async store({ request, response }: HttpContext) {
+    let { id, stock } = request.body()
+    const query = await ProductVariant.findByOrFail('id', id)
+    const newStock = Number(query.stock) + Number(stock)
+    await query.merge({ stock: newStock.toString() }).save()
+    return response.status(200).json({ message: 'Restock Saved!' })
+  }
 }
