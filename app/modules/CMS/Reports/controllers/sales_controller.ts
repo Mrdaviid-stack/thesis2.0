9a44a634981a91479ff94b91e86e3cf95268886b
Mark David Bogayan <mrkdvdbgyn@gmail.com>
2025-05-24 12:45:51 +0800

    bugfix: total sales null value

--- app/modules/CMS/Reports/controllers/sales_controller.ts
+++ app/modules/CMS/Reports/controllers/sales_controller.ts
@@ -5,24 +5,26 @@ import _ from 'lodash'
 import historyService from '../services/historyServices.js'
 
 export default class SalesController {
+  async index({ view, auth }: HttpContext) {
+    await historyService(auth.user?.firstname!, `View Sales Reports Page`)
+    return view.render('pages/cms/reports/sales/sales_index')
+  }
 
-    async index({ view, auth }: HttpContext) {
-        await historyService(auth.user?.firstname!, `View Sales Reports Page`)
-        return view.render('pages/cms/reports/sales/sales_index')
-    }
+  async generate({ request, response, auth }: HttpContext) {
+    let { start, end } = request.qs()
 
-    async generate({ request,response, auth }: HttpContext) {
-        let { start, end } = request.qs()
+    const transactionQuery = await Transaction.query().whereBetween('created_at', [
+      `${start} 00:00:00`,
+      `${end} 23:59:59`,
+    ])
 
-        const transactionQuery = await Transaction.query()
-            .whereBetween('created_at', [`${start} 00:00:00`, `${end} 23:59:59`])
-
-        const totalSales = _.sumBy(transactionQuery, (transaction) => parseInt(transaction.totalAmount))
-        await historyService(auth.user?.firstname!, `Generate Sales`)
-        return response.status(200).json({
-            totalSales: new Intl.NumberFormat('en-PH', { style: 'currency', currency: 'PHP'}).format(totalSales),
-            transactions: transactionQuery
-        })
-    }
-
+    const totalSales = _.sumBy(transactionQuery, (transaction) => Number(transaction.totalAmount))
+    await historyService(auth.user?.firstname!, `Generate Sales`)
+    return response.status(200).json({
+      totalSales: new Intl.NumberFormat('en-PH', { style: 'currency', currency: 'PHP' }).format(
+        totalSales !== null ? totalSales : 0
+      ),
+      transactions: transactionQuery,
+    })
+  }
 }
